3338b124a69ab612e56aac1f7ebbbb72b0de49a0
Cristiano Sobral <cssobral2013@gmail.com>
2025-08-08 19:03:26 -0300

    Upload files

---
+++ generate-woff-woff2.py
@@ -0,0 +1,284 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+
+srcPath  = './ttf/Piatnitsa-ExtraLight.ttf'
+woffPath = './web/Piatnitsa-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Piatnitsa-ExtraLightItalic.ttf'
+woffPath = './web/Piatnitsa-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Piatnitsa-Light.ttf'
+woffPath = './web/Piatnitsa-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Piatnitsa-LightItalic.ttf'
+woffPath = './web/Piatnitsa-LightItalic.woff'
+   
+
+    
+srcPath  = './ttf/Piatnitsa-Regular.ttf'
+woffPath = './web/Piatnitsa-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Piatnitsa-Italic.ttf'
+woffPath = './web/Piatnitsa-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Piatnitsa-Medium.ttf'
+woffPath = './web/Piatnitsa-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Piatnitsa-MediumItalic.ttf'
+woffPath = './web/Piatnitsa-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Piatnitsa-SemiBold.ttf'
+woffPath = './web/Piatnitsa-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Piatnitsa-SemiBoldItalic.ttf'
+woffPath = './web/Piatnitsa-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Piatnitsa-SemiBold.ttf'
+woffPath = './web/Piatnitsa-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Piatnitsa-SemiBoldItalic.ttf'
+woffPath = './web/Piatnitsa-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Piatnitsa-Bold.ttf'
+woffPath = './web/Piatnitsa-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Piatnitsa-BoldItalic.ttf'
+woffPath = './web/Piatnitsa-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Piatnitsa-ExtraBold.ttf'
+woffPath = './web/Piatnitsa-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Piatnitsa-ExtraBoldItalic.ttf'
+woffPath = './web/Piatnitsa-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+srcPath  = './ttf/Piatnitsa-Black.ttf'
+woffPath = './web/Piatnitsa-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Piatnitsa-BlackItalic.ttf'
+woffPath = './web/Piatnitsa-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)      
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+
+
+srcPath  = './ttf/Piatnitsa-ExtraLight.ttf'
+woff2Path = './web/Piatnitsa-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Piatnitsa-ExtraLightItalic.ttf'
+woff2Path = './web/Piatnitsa-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Piatnitsa-Light.ttf'
+woff2Path = './web/Piatnitsa-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Piatnitsa-LightItalic.ttf'
+woff2Path = './web/Piatnitsa-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+
+    
+srcPath  = './ttf/Piatnitsa-Regular.ttf'
+woff2Path = './web/Piatnitsa-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Piatnitsa-Italic.ttf'
+woff2Path = './web/Piatnitsa-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Piatnitsa-Medium.ttf'
+woff2Path = './web/Piatnitsa-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Piatnitsa-MediumItalic.ttf'
+woff2Path = './web/Piatnitsa-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Piatnitsa-SemiBold.ttf'
+woff2Path = './web/Piatnitsa-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Piatnitsa-SemiBoldItalic.ttf'
+woff2Path = './web/Piatnitsa-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Piatnitsa-SemiBold.ttf'
+woff2Path = './web/Piatnitsa-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Piatnitsa-SemiBoldItalic.ttf'
+woff2Path = './web/Piatnitsa-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Piatnitsa-Bold.ttf'
+woff2Path = './web/Piatnitsa-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Piatnitsa-BoldItalic.ttf'
+woff2Path = './web/Piatnitsa-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Piatnitsa-ExtraBold.ttf'
+woff2Path = './web/Piatnitsa-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Piatnitsa-ExtraBoldItalic.ttf'
+woff2Path = './web/Piatnitsa-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+    
+srcPath  = './ttf/Piatnitsa-Black.ttf'
+woff2Path = './web/Piatnitsa-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Piatnitsa-BlackItalic.ttf'
+woff2Path = './web/Piatnitsa-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)  
